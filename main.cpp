14c53a665ef1c77af1ed6202b2392549195b66b6
Jack Dalton <jackdalton@users.noreply.github.com>
2015-03-10 06:48:20 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstdio>
+std::string version = "0.1.5";
+bool scrub = false;
+#include "sysloop.h"
+
+using namespace std;
+
+void secureLoop(string op);
+void cls();
+int main() {
+    system("clear");
+    sys("initial");
+}
